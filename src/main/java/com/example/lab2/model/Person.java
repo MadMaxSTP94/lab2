3c88d7be73188863eb2c5b6e74780a2f314b697a
metal <mee>
2023-10-27 23:22:39 +0300

    version 1.0

---
+++ src/main/java/com/example/lab2/model/Person.java
@@ -0,0 +1,14 @@
+package com.example.lab2.model;
+
+public class Person {
+    private int height;
+    private int weight;
+    private float temperature;
+    private PressureCharacteristics pressureCharacteristics;
+    private int leikots;
+    private int soe;
+    private int mioglobin;
+    private int holesterine;
+
+
+}
---
+++ src/main/java/com/example/lab2/model/PressureCharacteristics.java
@@ -0,0 +1,9 @@
+package com.example.lab2.model;
+
+public class PressureCharacteristics {
+    private Long id;
+    private Long person_id;
+    private int pressureLow;
+    private int pressureHigh;
+    private int pulse;
+}
